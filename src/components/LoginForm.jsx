0dadb6fd118aa5f3e5f093c32ba3ebb692164133
Francisco Sabino <fdasabino@gmail.com>
2025-01-29 12:09:28 +0100

    refactor(LoginForm): enhance error handling and display logic

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { ErrorMessage, Field, Form, Formik } from "formik";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as Yup from "yup";
 
 const passwordRegex = /^(?=.*[A-Z])(?=.*[\W]).{8,}$/;
@@ -18,7 +18,17 @@ const initialValues = {
 };
 
 const LoginForm = () => {
-  const [error, setError] = useState("");
+  const [error, setError] = useState(null);
+  console.log("LoginForm", error);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError(null);
+      }, 5000);
+    }
+  }, [error]);
+
   // handle login
   const handleLogin = async (values) => {
     try {
@@ -28,25 +38,24 @@ const LoginForm = () => {
       });
 
       console.log(response);
-
-      if (response.status === 404) {
-        setError(response.data.message);
-        return;
-      }
-
-      if (response.status === 400) {
-        setError(response.data.message);
-        return;
-      }
     } catch (error) {
       console.log(error);
+      if (error.response.status === 404) {
+        setError("User not found");
+        return;
+      }
+
+      if (error.response.status === 400) {
+        setError("Invalid credentials");
+        return;
+      }
     }
   };
 
   return (
     <div>
       <h3>Login to continue</h3>
-      {error && <p>{error}</p>}
+      {error && <div>{error}</div>}
       <Formik
         initialValues={initialValues}
         validationSchema={loginValidation}
