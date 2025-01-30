34133476075a40d624d2439f866995601f5920e4
Francisco Sabino <fdasabino@gmail.com>
2025-01-30 10:00:30 +0100

    feat(Login): redirect authenticated users to home page

--- src/routes/auth/Login.jsx
+++ src/routes/auth/Login.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 import LoginForm from "../../components/LoginForm";
 
 const Login = () => {
+  const { user } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [navigate, user]);
+
   return (
     <div>
       <h3>Login</h3>
