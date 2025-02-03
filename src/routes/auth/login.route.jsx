dda9a5607f3953287e6993796b5a4f64819c0abd
Francisco Sabino <fdasabino@gmail.com>
2025-02-03 09:04:47 +0100

    refactor(routes): simplify imports and remove unused user redirection logic in login route

--- src/routes/auth/login.route.jsx
+++ src/routes/auth/login.route.jsx
@@ -1,18 +1,6 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router";
 import LoginForm from "../../components/login.form.compnent";
 
 const Login = () => {
-  const { user } = useSelector((state) => state.user);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (user) {
-      navigate("/");
-    }
-  }, [navigate, user]);
-
   return (
     <div>
       <h3>Login</h3>
